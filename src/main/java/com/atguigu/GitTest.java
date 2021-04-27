4b1fd45d4dbfc98ddb570885886a154f3901d343
lhw <lhw@sise.com>
2021-04-27 20:17:31 +0800

    second commit

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -1,7 +1,9 @@
 package com.atguigu;
 
 public class GitTest {
+
     public static void main(String[] args) {
-        System.out.println("hello Git");
+        System.out.println("hello Git1");
+        System.out.println("hello Git2");
     }
 }
